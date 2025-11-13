4ae9e6ab82f5074d7073432018170dacb51c04b3
Onais-Shafi <onaisbaloch42@gmail.com>
2025-11-13 17:33:53 +0500

    file added

---
+++ 9 Assingment/app.js
@@ -0,0 +1,69 @@
+// question no 1
+
+let value = 3.45214
+
+console.log("integer value"+" "+ value);
+console.log("Round nnumber",Math.round(value))
+console.log("Ceil number",Math.ceil(value))
+console.log("Floor nnumber",Math.floor(value))
+
+
+// question no 2
+
+
+let values = -3.65214
+
+console.log("integer value"+" "+ values);
+console.log("Round nnumber",Math.round(values))
+console.log("Ceil number",Math.ceil(values))
+console.log("Floor nnumber",Math.floor(values))
+
+
+// question no 3
+
+
+let num =  -4
+
+let nums = Math.abs(num)
+
+console.log("The value "+num +" is"+" "+ nums);
+
+
+// question no 4 
+
+let dice = 4
+
+console.log("the dice value",Math.floor(Math.random(dice)*6+1));
+
+
+
+ // question no 5 
+
+
+ let head = "head"
+
+ let tail = "tail"
+
+
+ console.log(Math.floor(Math.random()+1),head || tail);
+
+ // question no 6 
+
+
+ let randomNum = 100
+ let random = Math.floor(Math.random()*100)
+
+ console.log(random);
+
+
+ // question no 7 
+
+ let admin = prompt("Choose only one to the ten value")
+
+
+ console.log("your num" , "is " + admin);
+ 
+ console.log("numbers-->",Math.floor(Math.random()*10));
+ 
+ 
+ 
