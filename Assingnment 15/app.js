09799682c7a714b54068ec242c4c15b69a8d38a5
Onais-Shafi <onaisbaloch42@gmail.com>
2025-12-01 18:39:07 +0500

    file added

---
+++ Assingnment 15/app.js
@@ -0,0 +1,28 @@
+// Q1
+//Answer no1 
+
+function link(){
+    alert("click here")
+}
+
+
+// Q2
+//Answer no2
+
+function hawa() {
+    alert("hawawei Mobile")
+}
+
+function sams() {
+
+    alert("samsung Mobile")
+}
+
+
+
+// Q3
+//Answer no3
+
+function deleteRow(button) {
+        button.parentElement.parentElement.remove();
+    }
---
+++ login page/app.js
@@ -0,0 +1,8 @@
+let emailinp = document.getElementById("email")
+let passwardinp= document.getElementById("passward")
+
+function submit() {
+    console.log("email-->",emailinp.value);
+    console.log("passward-->",passwardinp.value);
+}
+submit() 
